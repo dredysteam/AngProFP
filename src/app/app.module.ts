fde04216fb0d487ee6943cfeeb2a318b1ff7f9b9
Edilberto Rios <dredysteam@gmail.com>
2021-05-03 11:38:01 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { Store } from '../../src/store';
+
+// feature modules
+
+
+// containers
 import { AppComponent } from './app.component';
+
+//components
 
 @NgModule({
   declarations: [
@@ -10,9 +17,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [Store],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
